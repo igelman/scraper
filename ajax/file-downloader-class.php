bac0c28548519509b70baadf3a0561263f4c8add
Alan Igelman <alan@igelman.com>
2013-09-11 10:43:25 -0400

    Continue refactoring of FileDownloader class.

--- ajax/file-downloader-class.php
+++ ajax/file-downloader-class.php
@@ -1,26 +1,97 @@
 <?php
 
 class FileDownloader {
+	private $urls;
+	public $mh;
+	public $curlHandlers;
+	
 	
 	public function __construct($urls) {
-		$this->urls = $urls;
+		$this->urls = $this->cleanUrlArray($urls);
 	}
 	
-	public function cleanUrlArray($urlArray) {
+	public function getUrls(){
+		return $this->urls;
+	}
+	
+	private function cleanUrlArray($urlArray) {
 		return array_values(array_unique($urlArray)); // de-dupe the array
 		// nned to add a check to make sure these are all proper urls, too
 	}
 
-	private function createCurlOptionsArray() {
+	private function createCurlOptionsArray($url) {
+	/**
+	Create array of curl options based on set of defaults and the url parameter
+	Input:
+		$url
+	Output:
+		Array of curl options
+	**/
 		$userAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6 (.NET CLR 3.5.30729)";
-		$this->defaultCurlOptions = array(
+		return array(
 			CURLOPT_RETURNTRANSFER => TRUE, // return content
 			CURLOPT_USERAGENT      => $userAgent, // set user-agent
 			CURLOPT_AUTOREFERER    => TRUE,
 			CURLOPT_FOLLOWLOCATION => TRUE, // follow redirects
-			CURLOPT_URL            => "", // this is where we'll put the target url
+			CURLOPT_URL            => $url, // this is where we'll put the target url
 		);
 	} // createCurlOptionsArray
+	
+	public function createCurlMultiHandler() {
+		$this->mh = curl_multi_init();
+		foreach ($this->urls as $i => $url) {
+			$this->curlHandlers[$i] = curl_init();
+			curl_setopt_array($this->curlHandlers[$i], $this->createCurlOptionsArray($url));
+			curl_multi_add_handle($this->mh, $this->curlHandlers[$i]);
+		}
+	} // createCurlMultiHandler
+	
+	private function executeMultiHandler(){
+		$running = null;
+		echo "****" . get_resource_type($this->mh) . "****";
+/*
+		do {
+			curl_multi_exec($this->mh, $running);
+		} while($running > 0);
+*/
+	}
+	
+	public function storeFiles() {
+		$this->executeMultiHandler();
+		foreach($this->curlHandlers as $ch) {
+			//handleCurlOutput($ch);
+			curl_multi_remove_handle($this->mh, $ch);
+		}
+	}
+	
+	private function handleCurlOutput($ch) {
+		$url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+		$urlBasename = pathinfo( parse_url( $url, PHP_URL_PATH ), PATHINFO_BASENAME );
+		
+		$fileStore = $GLOBALS['appRootPath'] . $GLOBALS['fileStorePath'] . time() . "-" . $urlBasename ;			
+		if ( ($html = curl_multi_getcontent($ch) ) === FALSE){ // check for empty output
+		// test length of retrieved file
+			$error = curl_error($ch);
+		}
+		if ( ($length = writeFile($fileStore, $html) ) === FALSE) {
+		// test length of written file
+			return "crap";
+		}	
+	}
+	
+	private function writeFile($fileStore, $html) {
+	/**
+	This method might be replaced by a DB insert.
+	Input:
+		$fileStore path to new file location
+		$html string of html
+	Side Effect:
+		Writes file to location $fileStore
+	Output:
+		Length of written file
+	**/
+		return file_put_contents($fileStore, $html)
+	}
 	
 	
 }
--- tests/test-file-downloader.php
+++ tests/test-file-downloader.php
@@ -5,6 +5,7 @@ class testFileDownloader extends PHPUnit_Framework_TestCase {
 
 	private $fd;
 	private $urls;
+	private $uniqueUrls;
 	
 	protected function setUp(){
 		$this->urls = array(
@@ -15,16 +16,26 @@ class testFileDownloader extends PHPUnit_Framework_TestCase {
 			"http://localhost/development/scraper/data/sample-files/gamestop-20130904-1140.html",
 			"http://localhost/development/scraper/data/sample-files/gap-20130904-1140.html",
 		);
+		$this->uniqueUrls = count(array_unique($this->urls)); // 3, i.e., number of unique urls in $urls
 		$this->fd = new FileDownloader($this->urls);
 	}
 
 	public function testConstruct() {
 		$this->assertInstanceOf('FileDownloader',$this->fd);
+		$this->assertEquals($this->uniqueUrls, count($this->fd->getUrls())); // constructor de-dupes array, so 3 unique items in the 6 item array
 	}
 	
-	public function testCleanUrlArray() {
-		$this->urls = $this->fd->cleanUrlArray($this->urls);	// remove duplicate items
-		$this->assertEquals(count($this->urls), 3); // of which there are 3 in the 6 item array
+	
+	public function testCreateCurlMultiHandler() {
+		$this->fd->createCurlMultiHandler();
+		$this->assertEquals("curl_multi", get_resource_type($this->fd->mh) );
+		$this->assertEquals($this->uniqueUrls, count($this->fd->curlHandlers));
+	}
+	
+	public function testStoreFiles() {
+		$this->fd->createCurlMultiHandler();
+		$this->fd->storeFiles();
+		$this->assertEquals("curl_multi", get_resource_type($this->fd->mh) );
 	}
 }
 
