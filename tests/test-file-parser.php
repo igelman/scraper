c3491e63a6bb3e19152095d3f786b1b12678cc4c
Alan Igelman <alan@igelman.com>
2014-03-13 14:04:19 -0400

    Add new test file with new dealnews.com mark-up. Make a few edits of the unholy mess that is test-file-parser.php.

--- tests/test-file-parser.php
+++ tests/test-file-parser.php
@@ -28,16 +28,16 @@ class TestFileParser extends PHPUnit_Framework_TestCase {
 		$this->rmnString = "<li id=\"c5016722\" class=\"offer clearfix coupon pop\" data-offerid=\"5016722\" data-couponscore=\"67\" data-couponrank=\"0.72\" data-couponid=\"parent.madreId\" data-siteid=\"137559\" data-storename=\"GameFly\" data-storedomain=\"gamefly.com\" data-titleslug=\"20-percent-off-digital-downloads\" ><div class=\"detail\"> <div class=\"description\"> <div class=\"title\"> <h3> 20% Off Digital Downloads </h3> </div> <div class=\"codelabel\">coupon code:</div> <div class=\"crux attachFlash\"> <div class=\"cover\">Show coupon code</div> <div class=\"label\">&nbsp;</div> <div class=\"code\">GFDSEP20</div> </div> <p class=\"discount\"> GameFly: Shop Now and Save 20% Off Digital Downloads with Coupon Code! Valid through 9/30/13 only. </p> </div> <ul class=\"offer_status\" data-expires=\"2013-09-30 23:59:59\" data-last-click=\"-4436000\" data-num-clicks-today=\"57\"> <li class=\"metadata1 border-right\">57 people used today</li> <li class=\"metadata2\">Last used 1 hour ago</li> </ul> <ul class=\"actions\"> <li class=\"commentsTrigger\"> <span class=\"commentBubble\">2</span> <span class=\"commentLink\">Comments</span> </li> <li class=\"shareTrigger faux-link\" >Share</li> </ul> </div> <div class=\"voting\"> <div class=\"rating high\"> <div class=\"percent\">67<span>%</span></div> <div class=\"success\">Success</div> </div> <div class=\"thumbs\"> <span class=\"up canVote\"><span class=\"tooltip\">good coupon</span></span> <span class=\"down canVote\"><span class=\"tooltip\">bad coupon</span></span> </div> <div class=\"vote_count\">3 votes</div> </div> <div class=\"offer_detail comment_detail\"> <div class=\"detail_close\"><span>X</span></div> <div class=\"comments\"> <div class=\"comment_count\">Showing 2 most recent comments</div> <ul class=\"comment_list\"> <li class=\"comment up\"> <p class=\"comment_description\"> Saved $17.00 on Rome Total War 2 (09/04/2013) </p> <p class=\"comment_author\"> - Anonymous, 13 hours ago</p> </li><li class=\"comment\"> <p class=\"comment_description\"> Awesome! Really worked. Thanks!!! </p> <p class=\"comment_author\"> - Anonymous, 13 hours ago</p> </li> </ul> </div> </div></li>";
 
 		
-		$this->rmnFileParser = FileParser::createFromFile("RetailmenotParser", $this->rmnFile);		
+//		$this->rmnFileParser = FileParser::createFromFile("RetailmenotParser", $this->rmnFile);		
 //		$this->rmnParser = FileParser::createFromHtml("RetailmenotParser", $this->rmnString);
-//		$this->dnFileParser = FileParser::createFromFile("DealnewsParser", $this->dnFile);
+		$this->dnParser = FileParser::createFromFile("DealnewsParser", $this->dnFile);
 
 	}
 	
 	public function testConstruct() {
-		$this->assertInstanceOf("RetailmenotParser", $this->rmnFileParser);
+//		$this->assertInstanceOf("RetailmenotParser", $this->rmnFileParser);
 //		$this->assertInstanceOf("RetailmenotParser", $this->rmnParser);
-//		$this->assertInstanceOf("DealnewsParser", $this->dnParser);
+		$this->assertInstanceOf("DealnewsParser", $this->dnParser);
 	}
 	
 	
